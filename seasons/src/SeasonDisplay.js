fcc25489e80fc2fad719133338ba9c56f5a89974
Alejandro <alejandropena17@gmail.com>
2021-05-26 15:30:51 -0400

    intro to react classes and state control.

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,8 @@
+import React from  'react';
+
+// Geolocation API, built in JS
+const SeasonDisplay = () => {
+    return <div>Season Display!</div>
+};
+
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// will determine location + month
+// const App = () => {
+//     // first param success callback, second failure callback
+//     // async
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     );
+
+//     return <div>Hi There</div>
+// };
+
+
+//valid class based component 1) JS class, extends(subclass react comp), render method returning JSX
+class App extends React.Component {
+    // js not requ
+    constructor(props) {
+        super(props); // overrriding constructor Fx of React.Component parent class
+
+        // only time we do direct assignment**
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //this.state.lat = x DOESNT WORK
+                // this calls to auto render
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => console.log(err)
+        );
+    }
+
+    // react required, called frequently*
+    render() {
+
+        return <div>Latitude: {this.state.lat}</div>
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
